2d820f0a7c4f4228a83ff7d0af4e0f093d30f416
sandra-03-kasira <kasirasandra@gmail.com>
2025-03-13 09:50:51 -0700

    Add files via upload

---
+++ 2dimenstional array.cpp
@@ -0,0 +1,22 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+	 int grade[6];
+	 int students [5];
+	int table[5][6]={{40, 50, 60,70,67,66},
+	                 {90, 50, 80, 5,70,20},
+					 {50, 6, 67,  7,56 ,78},
+					 {20, 30, 49,100, 34, 53},
+					 {30, 48, 55, 66, 77, 83 }};
+					 for(int row=0; row<6; row++ )
+					 
+                    for (int col = 0; col < 5; col++){
+					
+                   int total += table[row][col];}
+
+
+cin>>table[0][0];				 
+					 
+return 0;					 
+}
